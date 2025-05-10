878c87f2ec9f3bd79da2dfd3ee6da0065414a325
JoaoV-A01 <victor.joao01101010@gmail.com>
2025-05-09 23:50:40 -0300

    Design Pattern Strategy

---
+++ desafios/lab-padroes-projeto-java/src/one/digitalinnovation/gof/stategy/ComportamentoDefensivo.java
@@ -0,0 +1,9 @@
+package one.digitalinnovation.gof.stategy;
+
+public class ComportamentoDefensivo implements Comportamento {
+
+    @Override
+    public void mover() {
+        System.out.println("Movendo-se defensivamento ");
+    }
+}
